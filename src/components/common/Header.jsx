c0af34495a73ea01637975d7d8f716e6f034c37d
rapid trader <166290591+rapidtrader@users.noreply.github.com>
2026-05-16 10:18:00 +0530

    change the catalogue pdf

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -274,7 +274,7 @@ export default function Header() {
             {/* Catalog Download */}
             <li className="mt-2 md:mt-0 md:ml-4">
               <a
-                href="/Home/Catalog/Main.pdf"
+                href="/Home/Catalog/new-one.pdf"
                 target="_blank"
                 onClick={handleMenuLinkClick}
                 className="inline-block"
