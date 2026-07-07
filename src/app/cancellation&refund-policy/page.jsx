6c4c7753aa8268af8a12092a2fcdeebc7b979519
rapid trader <166290591+rapidtrader@users.noreply.github.com>
2026-07-07 18:51:53 +0530

    remove the perishable items

--- src/app/cancellation&refund-policy/page.jsx
+++ src/app/cancellation&refund-policy/page.jsx
@@ -9,22 +9,22 @@ DYNACLEAN INDUSTRIES PRIVATE LIMITED believes in helping its customers as far as
 
 Cancellations will be considered only if the request is made immediately after placing the order. However, the cancellation request may not be entertained if the orders have been communicated to the vendors/merchants and they have initiated the process of shipping them.`,
   },
+  // {
+  //   category: "1. Perishable Items",
+  //   content: `DYNACLEAN INDUSTRIES PRIVATE LIMITED does not accept cancellation requests for perishable items like flowers, eatables etc. However, refund/replacement can be made if the customer establishes that the quality of product delivered is not good.`,
+  // },
   {
-    category: "1. Perishable Items",
-    content: `DYNACLEAN INDUSTRIES PRIVATE LIMITED does not accept cancellation requests for perishable items like flowers, eatables etc. However, refund/replacement can be made if the customer establishes that the quality of product delivered is not good.`,
-  },
-  {
-    category: "2. Damaged or Defective Items",
+    category: "1. Damaged or Defective Items",
     content: `In case of receipt of damaged or defective items please report the same to our Customer Service team. The request will, however, be entertained once the merchant has checked and determined the same at his own end. 
 
 This should be reported within the same day of receipt of the products.`,
   },
   {
-    category: "3. Product Expectations",
+    category: "2. Product Expectations",
     content: `In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within the same day of receiving the product. The Customer Service Team after looking into your complaint will take an appropriate decision.`,
   },
   {
-    category: "4. Warranty & Refunds",
+    category: "3. Warranty & Refunds",
     content: `In case of complaints regarding products that come with a warranty from manufacturers, please refer the issue to them. 
 
 In case of any Refunds approved by the DYNACLEAN INDUSTRIES PRIVATE LIMITED, it’ll take 6-8 days for the refund to be processed to the end customer.`,
